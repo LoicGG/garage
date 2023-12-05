e89e4c5a292bc02b6a97d4f079394acac8cacd4a
LOIC GREBERT <92549640+LoicGG@users.noreply.github.com>
2023-12-05 15:57:25 +0100

    hamb

--- app.js
+++ app.js
@@ -7,8 +7,11 @@ var btn = document.getElementById("myBtn");
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
-// When the user clicks on the button, open the modal 
-btn.onclick = function() {
+// Get the hamburger element that opens the modal
+var menubar = document.getElementById("barmenu");
+
+// When the user clicks on the button, open the modal
+barmenu.onclick = function () {
   modal.style.display = "block";
 }
 
